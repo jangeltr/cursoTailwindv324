cfe60bd002aed0a40df7eb2cc98b4b0e2589283c
jangeltr <jangeltr@hotmail.com>
2023-02-02 14:47:51 +0000

    primer commit con Flowbite incluido

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/html/**/*.{html,js}"],
+  content: ["./src/html/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
