decc954bebca7e9615911542b47f49445bfaa30b
jangeltr <jangeltr@hotmail.com>
2023-02-07 14:44:47 +0000

    solo algunos cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/html/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  content: [
+    "./src/html/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"//para usar la libreria de componentes flowbite
+  ],
   theme: {
-    extend: {},
+    extend: {                       //Para configurar un sistema de estilos
+      backgroundImage:{             //Todo el conjunto de imagenes que serian utilizadas como background
+        'imagen1':'url("./public/img/MyPicture.jpg")',  
+        'imagen2':'url("./public/img/MyObjectives.png")'
+      },
+      backgroundColor: theme => ({  //Para definir colores a utilizar diferentes a los de tailwind 
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secundary': '#CC2D4A'
+      }),
+      textColor:{                   //Para definir los colores de los textos en nuestro sistema de estilos
+        'primary': '#CC2D4A',
+        'secundary': '#CC2D4A'
+      },
+      fontFamily:{                  //Las fuentes de nuestro sistema de estilos
+        Monserrat:[                 //primero obtener la fuente de       https://fonts.google.com/
+          'Monserrat',              //y desues se debe agregar la fuente obtenida al    index.html
+          'sans-serif'              //por ultimo agregar aqui en tailwind.config.js
+        ]
+      }
+    },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin')      //para usar la libreria de componentes flowbite
   ],
 }
